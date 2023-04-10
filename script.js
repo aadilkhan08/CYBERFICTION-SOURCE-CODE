23584897eac178c1e4c2076cdc93d3845e64d256
Aadil <aadilmoid@gmail.com>
2023-04-10 13:43:56 +0530

    another commit

--- script.js
+++ script.js
@@ -421,7 +421,6 @@ gsap.to("#page1",{
     trigger:`#page1`,
     start:`top top`,
     end:`bottom top`,
-    markers:true,
     pin:true,
     scroller:`#main`
   }
@@ -431,7 +430,6 @@ gsap.to("#page2",{
     trigger:`#page2`,
     start:`top top`,
     end:`bottom top`,
-    markers:true,
     pin:true,
     scroller:`#main`
   }
@@ -441,7 +439,6 @@ gsap.to("#page3",{
     trigger:`#page3`,
     start:`top top`,
     end:`bottom top`,
-    markers:true,
     pin:true,
     scroller:`#main`
   }
